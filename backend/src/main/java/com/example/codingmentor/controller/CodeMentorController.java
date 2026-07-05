8d0b529c73f5078b726a96f84d10da2bc37b3b83
Sainath52 <k60050852@gmail.com>
2026-07-05 12:48:08 +0530

    Update CodeMentorController

--- backend/src/main/java/com/example/codingmentor/controller/CodeMentorController.java
+++ backend/src/main/java/com/example/codingmentor/controller/CodeMentorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/mentor")
-@CrossOrigin(origins = "http://localhost:5173") // Frontend to Backend Network Error (CORS) varama thaduka
+@CrossOrigin(origins = "https://capable-prosperity-production-5716.up.railway.app") // Frontend to Backend Network Error (CORS) varama thaduka
 public class CodeMentorController {
 
     private final CodeMentorService service;
